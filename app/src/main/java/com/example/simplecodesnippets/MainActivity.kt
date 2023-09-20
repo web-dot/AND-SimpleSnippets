e1c577282f626e83c395f68492b893c5fc60df69
sudipto <cg.sudipc@gmail.com>
2023-09-20 15:14:42 +0530

    text view test

--- app/src/main/java/com/example/simplecodesnippets/MainActivity.kt
+++ app/src/main/java/com/example/simplecodesnippets/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.simplecodesnippets
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.TextView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        // snippet 1(TextView test)
+        val textView1: TextView = findViewById(R.id.textView_one)
+        val textView2: TextView = findViewById(R.id.textView_two)
+        val textView3: TextView = findViewById(R.id.textView_three)
+
+        /**
+        textView1.setOnClickListener(object : View.OnClickListener {
+            override fun onClick(v: View?) {
+                val toast = Toast.makeText(this@MainActivity, "first", Toast.LENGTH_SHORT)
+                toast.show()
+            }
+        })
+        */
+        // the above code can be written using a lambda expression as below
+        textView1.setOnClickListener{
+            val toast = Toast.makeText(this, "This is the first text", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
+        textView2.setOnClickListener{
+            val toast = Toast.makeText(this, "This is the second text", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+
+        textView3.setOnClickListener{
+            textView3.setText("Hello from HelpDesk")
+        }
+
+
+
     }
 }
