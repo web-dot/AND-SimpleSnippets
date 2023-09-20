b1bce713a3cd46d433000c60987e49d4a70d33d7
sudipto <cg.sudipc@gmail.com>
2023-09-20 16:52:27 +0530

    added TextView, EditText, ImageView snippets

--- app/src/main/java/com/example/simplecodesnippets/MainActivity.kt
+++ app/src/main/java/com/example/simplecodesnippets/MainActivity.kt
@@ -3,8 +3,11 @@ package com.example.simplecodesnippets
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import android.widget.EditText
+import android.widget.ImageView
 import android.widget.TextView
 import android.widget.Toast
+import androidx.core.widget.addTextChangedListener
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,18 +15,18 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         // snippet 1(TextView test)
+        /**
         val textView1: TextView = findViewById(R.id.textView_one)
         val textView2: TextView = findViewById(R.id.textView_two)
         val textView3: TextView = findViewById(R.id.textView_three)
 
-        /**
         textView1.setOnClickListener(object : View.OnClickListener {
             override fun onClick(v: View?) {
                 val toast = Toast.makeText(this@MainActivity, "first", Toast.LENGTH_SHORT)
                 toast.show()
             }
         })
-        */
+
         // the above code can be written using a lambda expression as below
         textView1.setOnClickListener{
             val toast = Toast.makeText(this, "This is the first text", Toast.LENGTH_SHORT)
@@ -39,7 +42,28 @@ class MainActivity : AppCompatActivity() {
             textView3.setText("Hello from HelpDesk")
         }
 
+        */
 
+        // snippet 2(Edit Text)
+        /**
+        val textViewForEditText: TextView = findViewById(R.id.textView)
+        val editText: EditText = findViewById(R.id.editText)
+
+        editText.addTextChangedListener{
+            val name1 = editText.getText().toString()
+            textViewForEditText.setText("Hello " + name1)
+        }
+        */
+
+
+        // snippet 3(Image View)
+        /**
+        val imageView: ImageView = findViewById(R.id.imageView)
+        imageView.setOnClickListener{
+            val toast = Toast.makeText(this, "Hello From Android", Toast.LENGTH_SHORT)
+            toast.show()
+        }
+        */
 
     }
 }
